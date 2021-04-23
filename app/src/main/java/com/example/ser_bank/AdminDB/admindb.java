b19f5a6aa53a27cd860e1be83e525726b892407e
Johan Viancha <vianchajohan@gmail.com>
2021-04-23 09:05:38 -0500

    Creación de estructura de la base de datos

---
+++ app/src/main/java/com/example/ser_bank/AdminDB/admindb.java
@@ -0,0 +1,51 @@
+package com.example.ser_bank.AdminDB;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class admindb extends SQLiteOpenHelper {
+
+    public admindb(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE usuario (" +
+                "id_usuario integer primary key," +
+                "nombre_usu text," +
+                "apellidos_usu text," +
+                "email_usu text," +
+                "password_usu text," +
+                "tipo_usu text" +
+                ")");
+
+        db.execSQL("CREATE TABLE cuenta(" +
+                "id_cuenta integer primary key," +
+                "id_usu integer," +
+                "codigo_cue text," +
+                "saldo_cue money," +
+                "tipo_cue text," +
+                "foreign key (id_usu) references usuario (id_usuario))");
+
+        db.execSQL("CREATE TABLE transaccion(" +
+                "id_transacion integer primary key," +
+                "id_cue_emi integer," +
+                "id_cue_rec integer," +
+                "tipo_trans text," +
+                "monto_trans money," +
+                "foreign key (id_cue_emi) references cuenta (id_cuenta)," +
+                "foreign key (id_cue_rec) references cuenta (id_cuenta))");
+
+
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
